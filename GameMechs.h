a8fa9f90f93507ca789432f2ae307c64461a7ed7
fuz45 <fuz45@mcmaster.ca>
2024-11-25 20:20:51 -0500

    format changes

--- GameMechs.h
+++ GameMechs.h
@@ -1,49 +1,51 @@
 #ifndef GAMEMECHS_H
 #define GAMEMECHS_H
 
-#include <cstdlib>
 #include <time.h>
+
+#include <cstdlib>
 
 #include "objPos.h"
 #include "objPosArrayList.h"
 
 using namespace std;
 
-class GameMechs
-{
-    private:
-        char input;
-        bool exitFlag;
-        bool loseFlag;
-        int score;
+class GameMechs {
+   private:
+    char input;
+    bool exitFlag;
+    bool loseFlag;
+    int score;
 
-        int boardSizeX;
-        int boardSizeY;
-        char ** board;
+    int boardSizeX;
+    int boardSizeY;
 
-        objPos food;
+    objPos food;
 
-    public:
-        GameMechs();
-        GameMechs(int boardX, int boardY);
-        ~GameMechs(); // is this one needed at all? Why or why not?
-        
-        bool getExitFlagStatus() const; 
-        void setExitTrue();
-        bool getLoseFlagStatus() const;
-        void setLoseFlag();
+    // custom
+    char** board;
 
-        char getInput() const;
-        void setInput(char this_input);
-        void clearInput();
+   public:
+    GameMechs();
+    GameMechs(int boardX, int boardY);
+    ~GameMechs();  // is this one needed at all? Why or why not?
 
-        int getBoardSizeX() const;
-        int getBoardSizeY() const;
-        
-        int getScore() const;
-        void incrementScore();
-        
-        // More methods should be added here
+    bool getExitFlagStatus() const;
+    void setExitTrue();
+    bool getLoseFlagStatus() const;
+    void setLoseFlag();
+
+    char getInput() const;
+    void setInput(char this_input);
+    void clearInput();
+
+    int getBoardSizeX() const;
+    int getBoardSizeY() const;
+
+    int getScore() const;
+    void incrementScore();
+
+    // More methods should be added here
 };
 
 #endif
