c7fa0ac5d3b3bccb11e313753598cffd39c78797
fuz45 <fuz45@mcmaster.ca>
2024-11-27 14:45:33 -0500

    testsuite update

--- objPosArrayList_TestSuite/objPosArrayList.cpp
+++ objPosArrayList_TestSuite/objPosArrayList.cpp
@@ -1,54 +1,60 @@
 #include "objPosArrayList.h"
 
-// Check lecture contents on general purpose array list construction, 
+// Check lecture contents on general purpose array list construction,
 // and modify it to support objPos array list construction.
 
-objPosArrayList::objPosArrayList()
-{
-    
+objPosArrayList::objPosArrayList() {
+    arrayCapacity = ARRAY_MAX_CAP;
+    listSize = 0;
+    aList = new objPos[arrayCapacity];
 }
 
-objPosArrayList::~objPosArrayList()
-{
-
+objPosArrayList::~objPosArrayList() {
+    delete[] aList;
 }
 
-int objPosArrayList::getSize() const
-{
-
+int objPosArrayList::getSize() const {
+    return listSize;
 }
 
-void objPosArrayList::insertHead(objPos thisPos)
-{
-    
+void objPosArrayList::insertHead(objPos thisPos) {
+    if (listSize >= arrayCapacity) return;
+    for (int i = listSize; i > 0; --i) {
+        aList[i] = aList[i - 1];
+    }
+    aList[0] = thisPos;
+    ++listSize;
 }
 
-void objPosArrayList::insertTail(objPos thisPos)
-{
-    
+void objPosArrayList::insertTail(objPos thisPos) {
+    if (listSize < arrayCapacity) {
+        aList[listSize++] = thisPos;
+    }
 }
 
-void objPosArrayList::removeHead()
-{
-    
+void objPosArrayList::removeHead() {
+    if (listSize > 0) {
+        for (int i = 0; i < listSize - 1; ++i) {
+            aList[i] = aList[i + 1];
+        }
+        --listSize;
+    }
 }
 
-void objPosArrayList::removeTail()
-{
-    
+void objPosArrayList::removeTail() {
+    if (listSize > 0) {
+        --listSize;
+    }
 }
 
-objPos objPosArrayList::getHeadElement() const
-{
-    
+objPos objPosArrayList::getHeadElement() const {
+    return aList[0];
 }
 
-objPos objPosArrayList::getTailElement() const
-{
-    
+objPos objPosArrayList::getTailElement() const {
+    return aList[listSize - 1];
 }
 
-objPos objPosArrayList::getElement(int index) const
-{
-    
+objPos objPosArrayList::getElement(int index) const {
+    return (index >= 0 && index < listSize) ? aList[index] : objPos();
 }
